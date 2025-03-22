7e54772d3c7a2bb9172aae2d5bd6182274a5e47b
Worky13 <39780939+Worky13@users.noreply.github.com>
2025-03-22 17:31:55 +0000

    commit Nivel Aventureirodo xadrez finalizada

---
+++ Aula Estrutura de Repetição/Loops Alinhados/Exemplo_em_for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+Nesse exemplo, temos um loop for dentro de outro loop for. O loop externo controla as linhas (multiplicando), 
+e o loop interno controla as colunas (multiplicador). Para cada valor de i (de 1 a 10), o loop interno percorre 
+j (de 1 a 10), e, em cada iteração do loop interno, a multiplicação de i e j é impressa.
+*/
